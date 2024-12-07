b7310208d5064c7644701467d59add784c1541e0
Kenzo <brandikenzo@gmail.com>
2024-12-07 16:05:21 +0100

    fdsf

--- DevCard.java
+++ DevCard.java
@@ -35,7 +35,7 @@ public class DevCard implements Displayable {
                             "\u2514\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2518"};
         //update cost of the repr
         int i=6;
-        for(Resource res: cost){ //-- parcourir l'ensemble des resources (res)en utilisant l'énumération Resource
+        for(Resource res: getCost().keySet()){ //-- parcourir l'ensemble des resources (res)en utilisant l'énumération Resource
             if(getCost().getNbResource(res)>0){
                 cardStr[i] = "\u2502"+getCost().getNbResource(res)+" "+res.toSymbol()+"    \u2502";
                 i--;
@@ -69,16 +69,13 @@ public class DevCard implements Displayable {
 
     public String toString(){
         String cardStr = "";
-        /*
-         * Ce code est à décommenter une fois que la classe DevCard a été implémentée
               
         cardStr = getPoints()+"pts, type "+resourceType.toSymbol()+" | coût: ";
-        for(ACOMPLETER){ //-- parcourir l'ensemble des resources (res) en utilisant l'énumération Resource
+        for(Resource res: getCost().keySet()){ //-- parcourir l'ensemble des resources (res) en utilisant l'énumération Resource
             if(getCost().getNbResource(res)>0){
                 cardStr += getCost().getNbResource(res)+res.toSymbol()+" ";
             }
         }
-        */
         return cardStr;
     }
     
