07043fd9ed2e38bbc3c7393ab2267c282336c724
Oceanet31 <oceane.toto.lemene@gmail.com>
2024-12-15 16:17:20 +0100

    Add files via upload

---
+++ Splender_FINAL PROJECT/DevCard.java
@@ -0,0 +1,99 @@
+ 
+
+import java.util.ArrayList;
+public class DevCard implements Displayable {
+    private int level;
+    private Resources cost;
+    private int points;
+    private Resource resourceType;
+    public DevCard(int niveau,Resources cout,int pointsPrestige,Resource bonus){
+        level = niveau;
+        cost = cout;
+        points = pointsPrestige;
+        resourceType = bonus;
+    }
+    public String[] toStringArray(){
+        /** EXAMPLE
+         * ┌────────┐
+         * │①    ♠S│
+         * │        │
+         * │        │
+         * │2 ♠S    │
+         * │2 ♣E    │
+         * │3 ♥R    │
+         * └────────┘
+         */
+        String pointStr = "  ";
+        if(getPoints()>0){
+            pointStr = new String(new int[] {getPoints()+9311}, 0, 1);
+        }
+        String[] cardStr = {"\u250C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2510",
+                            "\u2502"+pointStr+"    "+resourceType.toSymbol()+"\u2502",
+                            "\u2502        \u2502",
+                            "\u2502        \u2502",
+                            "\u2502        \u2502",
+                            "\u2502        \u2502",
+                            "\u2502        \u2502",
+                            "\u2514\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2518"};
+        //update cost of the repr
+        int i=6;
+        for(Resource res: getCost().keySet()){ //-- parcourir l'ensemble des resources (res)en utilisant l'énumération Resource
+            if(getCost().getNbResource(res)>0){
+                cardStr[i] = "\u2502"+getCost().getNbResource(res)+" "+res.toSymbol()+"    \u2502";
+                i--;
+            }
+        } 
+        return cardStr;
+    }
+
+    public static String[] noCardStringArray(){
+        /** EXAMPLE
+         * ┌────────┐
+         * │ \    / │
+         * │  \  /  │
+         * │   \/   │
+         * │   /\   │
+         * │  /  \  │
+         * │ /    \ │
+         * └────────┘
+         */
+        String[] cardStr = {"\u250C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2510",
+                            "\u2502 \\    / \u2502",
+                            "\u2502  \\  /  \u2502",
+                            "\u2502   \\/   \u2502",
+                            "\u2502   /\\   \u2502",
+                            "\u2502  /  \\  \u2502",
+                            "\u2502 /    \\ \u2502",
+                            "\u2514\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2518"};
+        
+        return cardStr;
+    }
+
+    public String toString(){
+        String cardStr = "";
+              
+        cardStr = getPoints()+"pts, type "+resourceType.toSymbol()+" | coût: ";
+        for(Resource res: getCost().keySet()){ //-- parcourir l'ensemble des resources (res) en utilisant l'énumération Resource
+            if(getCost().getNbResource(res)>0){
+                cardStr += getCost().getNbResource(res)+res.toSymbol()+" ";
+            }
+        }
+        return cardStr;
+    }
+    
+    public int getLevel(){
+        return level;
+    }
+    
+    public Resources getCost(){
+    return cost;
+    }
+
+    public int getPoints(){
+    return points;
+    }
+
+    public Resource getRessourceType(){
+    return resourceType;
+    }
+}
